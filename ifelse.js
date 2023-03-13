58e264c6bb976a1282971328b600e6bfcdf7e736
SasiwanJ <119156708+SasiwanJ@users.noreply.github.com>
2023-03-13 07:02:42 +0000

    add switch

---
+++ ifelse.js
@@ -0,0 +1,32 @@
+let dayNumber = Nunber(prompt("Enter day number:"));
+
+let dayOff;
+
+switch (dayNumber) {
+    case 0:
+        dayOff = "Sunday";
+        break;
+    case 1:
+        dayOff = "Monday";
+        break;
+    case 2:
+        dayOff = "Tuesday";
+        break;
+    case 3:
+        dayOff = "Wednesday";
+        break;
+    case 4:
+        dayOff = "Thursday";
+        break;
+    case 5:
+        dayOff = "Friday";
+        break;
+    case 5:
+        dayOff = "Saturday";
+        break;
+    
+    default:
+        dayOff = "Invalid day number";
+
+    
+}
